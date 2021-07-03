5eb428c7b3372fd8c969b9ee4e5519fd78f4bd00
Jose Juan Mora <josejuanmora@MacBook-Pro-de-Jose.local>
2021-07-03 17:22:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useTable} from 'react-table'
+import styled from 'styled-components'
 
-function App() {
+const Styles = styled.div `
+  table {
+    width: 100%;
+    border-spacing: 0;
+    border: 1px solid black;
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+    th,
+    td {
+      margin: 0;
+      padding: 1rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`
+
+function Table({columns, data}) {
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data})
+
+  // Render Data Table UI
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup
+              .headers
+              .map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row
+                .cells
+                .map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
-export default App;
+function App() {
+  const data = [
+    {
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      age: 28,
+      status: 'Active'
+    },
+    {
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      age: 35,
+      status: 'Active'
+    },
+    {
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      age: 33,
+      status: 'Inactive'
+    },
+    {
+      name: 'Patricia Lebsack',
+      email: 'Julianne@kory.org',
+      age: 25,
+      status: 'Active'
+    },
+    {
+      name: 'Kamren',
+      email: 'Hettinger@annie.ca',
+      age: 42,
+      status: 'Active'
+    },
+    {
+      name: 'Dennis Schulist',
+      email: 'Dach@jasper.info',
+      age: 34,
+      status: 'Inactive'
+    },
+    {
+      name: 'Kurtis Weissnat',
+      email: 'Hoeger@billy.biz',
+      age: 44,
+      status: 'Active'
+    },
+    {
+      name: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      age: 26,
+      status: 'Active'
+    },
+    {
+      name: 'Glenna Reichert',
+      email: 'McDermott@dana.io',
+      age: 30,
+      status: 'Inactive'
+    },
+  ]
+
+  const columns = [
+    {
+      Header: 'Name',
+      accessor: 'name'
+    }, {
+      Header: 'Email',
+      accessor: 'email'
+    }, {
+      Header: 'Age',
+      accessor: 'age'
+    }, {
+      Header: 'Status',
+      accessor: 'status'
+    }
+  ]
+
+  return (
+    <Styles>
+      <Table
+        data={data}
+        columns={columns}
+      />
+    </Styles>
+  )
+
+}
+
+export default App
