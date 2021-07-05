2889be0edce695fb8e02351695054a0e2f09d645
Jose Juan Mora <josejuanmora@MacBook-Pro-de-Jose.local>
2021-07-05 15:18:01 +0200

    added request from pirates api

--- src/views/Ships.js
+++ src/views/Ships.js
@@ -1,62 +1,63 @@
-import React from "react";
-import Axios from "axios";
+import React from 'react';
+import axios from 'axios';
 import { Container, Row, Col, Card, CardHeader, CardBody } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
 
-const myArray = ['Jack', 'Mary', 'John', 'Krish', 'Navin'];
+class Ships extends React.Component {
+    constructor(props) {
+        super(props);
 
-const Ships = () => (
-  <Container fluid className="main-content-container px-4">
-    {/* Default Light Table */}
-    <Row>
-      <Col>
-        <Card small className="mb-4">
-          <CardHeader className="border-bottom">
-            <h6 className="m-0">Ships</h6>
-          </CardHeader>
-          <CardBody className="p-0 pb-3">
-            <table className="table mb-0">
-              <thead className="bg-light">
-                <tr>
-                  <th scope="col" className="border-0">
-                    #
-                  </th>
-                  <th scope="col" className="border-0">
-                    Ship Name
-                  </th>
-                  <th scope="col" className="border-0">
-                    Last Name
-                  </th>
-                  <th scope="col" className="border-0">
-                    Country
-                  </th>
-                  <th scope="col" className="border-0">
-                    City
-                  </th>
-                  <th scope="col" className="border-0">
-                    Phone
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {myArray.map(name => (
-                <tr>
-                  <td>{name}</td>
-                  <td>Ali</td>
-                  <td>Kerry</td>
-                  <td>Russian Federation</td>
-                  <td>Gdańsk</td>
-                  <td>107-0339</td>
-                </tr>
-                ))}
-              </tbody>
-            </table>
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-);
+        this.state = {
+            shipList: []
+        };
+    }
 
-export default Ships;
+    componentDidMount() {
+        axios.get('http://localhost:8080/api/v1_0/ship/')
+            .then(response => this.setState({ shipList: response.data }));
+    }
+
+    render() {
+        const { totalReactPackages } = this.state;
+        return (
+          <Container fluid className="main-content-container px-4">
+            <Row>
+              <Col>
+                <Card small className="mb-4">
+                  <CardHeader className="border-bottom">
+                    <h6 className="m-0">Ships</h6>
+                  </CardHeader>
+                  <CardBody className="p-0 pb-3">
+                    <table className="table mb-0">
+                      <thead className="bg-light">
+                        <tr>
+                          <th scope="col" className="border-0">
+                            #
+                          </th>
+                          <th scope="col" className="border-0">
+                            Ship Name
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      {this.state.shipList.map((item) => {
+                        return (
+                        <tr>
+                          <td>{item.id}</td>
+                          <td>{item.name}</td>
+                        </tr>
+                        );
+                      })}
+                      </tbody>
+                    </table>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        );
+    }
+}
+
+export default Ships ;
